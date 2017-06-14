3a0cf3b31a91f4635a9592859cba01dc8c0abeb8
Draco Whitefire <draco@dracowhitefire.net>
2017-06-14 19:15:27 +0200

    refactored

--- main.js
+++ main.js
@@ -1,35 +1,211 @@
-(function(){
-  let clear = document.querySelectorAll('.clear').forEach(function(element){
-    element.addEventListener("click", function clear(){
-      let input = document.getElementById('outputInput');
-      input.value = "";
-    });
-  });
+class Calculator {
+    /**
+     * @param $a
+     * @param $b
+     * @returns number
+     */
+    calculate($a, $b) {
+        return this.calculation.calculate($a, $b);
+    }
 
+    setCalculation($calculation) {
+        this.calculation = $calculation;
+    }
+}
 
+class SumCalculation {
+    /**
+     * @param $a
+     * @param $b
+     * @returns number
+     */
+    calculate($a, $b) {
+        return Number($a) + Number($b);
+    }
+}
 
+class SubtractCalculation {
+    /**
+     * @param $a
+     * @param $b
+     * @returns number
+     */
+    calculate($a, $b) {
+        return Number($a) - Number($b);
+    }
+}
 
-  let number = document.querySelectorAll('.number').forEach(function(element){
-    element.addEventListener("click", function number(){
-      let input = document.getElementById('outputInput');
-      let numberVal = this.textContent;
-      input.value += numberVal;
-    });
-  });
+class DivideCalculation {
+    /**
+     * @param $a
+     * @param $b
+     * @returns number
+     */
+    calculate($a, $b) {
+        return Number($a) / Number($b);
+    }
+}
 
-  let equal = document.querySelectorAll('.equal').forEach(function(element){
-    element.addEventListener("click", function equal(){
-      let input = document.getElementById('outputInput').value;
-      let array = input.split('');
-      for (i = 0; i < array.length; i++){
-        if(array[i] == "x"){
-          array[i] = "*";
+class ProductCalculation {
+    /**
+     * @param $a
+     * @param $b
+     * @returns number
+     */
+    calculate($a, $b) {
+        return Number($a) * Number($b);
+    }
+}
+
+class BufferManager {
+    constructor() {
+        this.activeBuffer = 'A';
+    }
+
+    clearBuffers() {
+        this.bufferA = null;
+        this.bufferB = null;
+    }
+
+    setActiveBuffer(value) {
+        switch (value) {
+            case 'A':
+                this.activeBuffer = 'A';
+                break;
+            case 'B':
+                this.activeBuffer = 'B';
+                break;
         }
-      }
-      array = array.join("");
-      let solution = eval(array);
-      document.getElementById('outputInput').value = solution;
+    }
+
+    addToBuffer(number) {
+        let allowed = /[\d.,]/;
+        if (!allowed.test(number)) {
+            return;
+        }
+        switch (this.activeBuffer) {
+            case 'A':
+                if (typeof this.bufferA === 'undefined' || this.bufferA === null) {
+                    this.bufferA = number;
+                } else {
+                    this.bufferA += number;
+                }
+                this.bufferA = Number(this.bufferA);
+                break;
+            case 'B':
+                if (typeof this.bufferB === 'undefined' || this.bufferB === null) {
+                    this.bufferB = number;
+                } else {
+                    this.bufferB += number;
+                }
+                this.bufferB = Number(this.bufferB);
+                break;
+        }
+    }
+
+    setToBuffer(buffer, value) {
+        switch (buffer) {
+            case 'A':
+                this.bufferA = value;
+                break;
+            case 'B':
+                this.bufferB = value;
+                break;
+        }
+    }
+
+    getActiveBufferContent() {
+        switch (this.activeBuffer) {
+            case 'A':
+                return this.bufferA;
+            case 'B':
+                return this.bufferB;
+        }
+    }
+
+    getInactiveBufferContent() {
+        switch (this.activeBuffer) {
+            case 'A':
+                return this.bufferB;
+            case 'B':
+                return this.bufferA;
+        }
+    }
+}
+
+(function () {
+    let bufferManager = new BufferManager();
+    let calculator = new Calculator();
+
+    let displayBuffers = function() {
+        let active = document.getElementById('active');
+        active.textContent = bufferManager.getActiveBufferContent();
+        let inactive = document.getElementById('inactive');
+        inactive.textContent = bufferManager.getInactiveBufferContent();
+    };
+
+    document.querySelectorAll('.clear').forEach(function (element) {
+        element.addEventListener("click", function clear() {
+            bufferManager.clearBuffers();
+            displayBuffers();
+        });
     });
-  });
+
+
+    document.querySelectorAll('.number').forEach(function (element) {
+        element.addEventListener("click", function number() {
+            let numberVal = this.textContent;
+            bufferManager.addToBuffer(numberVal);
+            displayBuffers();
+        });
+    });
+
+
+    document.querySelectorAll('.operator').forEach(function (element) {
+        element.addEventListener("click", function number() {
+            if (bufferManager.activeBuffer === 'B') {
+                let result = calculator.calculate(
+                    bufferManager.getInactiveBufferContent(),
+                    bufferManager.getActiveBufferContent()
+                );
+                bufferManager.clearBuffers();
+                bufferManager.setToBuffer('A', result);
+            } else {
+                bufferManager.activeBuffer = 'B';
+            }
+            displayBuffers();
+
+            let operator = this.textContent;
+            document.getElementById('operator').textContent = operator;
+            switch (operator) {
+                case '+':
+                    calculator.setCalculation(new SumCalculation());
+                    break;
+                case '-':
+                    calculator.setCalculation(new SubtractCalculation());
+                    break;
+                case 'x':
+                    calculator.setCalculation(new ProductCalculation());
+                    break;
+                case '/':
+                    calculator.setCalculation(new DivideCalculation());
+                    break;
+            }
+        });
+    });
+
+    document.querySelectorAll('.equal').forEach(function (element) {
+        element.addEventListener("click", function equal() {
+            let result = calculator.calculate(
+                bufferManager.getInactiveBufferContent(),
+                bufferManager.getActiveBufferContent()
+            );
+            bufferManager.clearBuffers();
+            bufferManager.setActiveBuffer('A');
+            bufferManager.setToBuffer('A', result);
+            displayBuffers();
+            document.getElementById('operator').textContent = '';
+        });
+    });
 
 })();
